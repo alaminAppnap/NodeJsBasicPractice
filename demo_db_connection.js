161903aa624a9f0eba28cc754eac9544e79f3636
alaminAppnap <alamin@appnap.io>
2022-09-06 16:24:44 +0600

    db query

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -7,14 +7,20 @@ var con = mysql.createConnection({
   database:"mydb"
 });
 
+
 /**========================== db connection =============================*/
+
 
 // con.connect(function(err) {
 //   if (err) throw err;
 //   console.log("Connected!");
 // });
 
+
+
 /**========================= create db ================================ */
+
+
 
 // con.connect(function(err) {
 //     if (err) throw err;
@@ -26,7 +32,11 @@ var con = mysql.createConnection({
 //   });
 
 
+
+
   /**=========================== create db table =============================*/
+
+
 
 //   con.connect(function(err) {
 //     if (err) throw err;
@@ -38,7 +48,12 @@ var con = mysql.createConnection({
 //     });
 //   });
 
+
+
+
   /**============================= Mysql Insert =====================================*/
+
+
 
 //   con.connect(function(err) {
 //     if (err) throw err;
@@ -52,30 +67,145 @@ var con = mysql.createConnection({
 
 
 
+
+
 /**============================== Mysql Multiple data insert ==========================================*/
+
+
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql = "INSERT INTO customers (name, address) VALUES ?";
+//     var values = [
+//       ['John', 'Highway 71'],
+//       ['Peter', 'Lowstreet 4'],
+//       ['Amy', 'Apple st 652'],
+//       ['Hannah', 'Mountain 21'],
+//       ['Michael', 'Valley 345'],
+//       ['Sandy', 'Ocean blvd 2'],
+//       ['Betty', 'Green Grass 1'],
+//       ['Richard', 'Sky st 331'],
+//       ['Susan', 'One way 98'],
+//       ['Vicky', 'Yellow Garden 2'],
+//       ['Ben', 'Park Lane 38'],
+//       ['William', 'Central st 954'],
+//       ['Chuck', 'Main Road 989'],
+//       ['Viola', 'Sideway 1633']
+//     ];
+//     con.query(sql, [values], function (err, result) {
+//       if (err) throw err;
+//       console.log("Number of records inserted: " + result.affectedRows);
+//     });
+//   });
+
+
+
+/**================ Select Query  ================== */
+
+
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     con.query("SELECT * FROM customers", function (err, result, fields) {
+//       if (err) throw err;
+//       console.log(result);
+//     });
+//   });
+
+
+
+
+/**================ Select Where Query  ================== */
+
+
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     con.query("SELECT * FROM customers where name = 'Peter'", function (err, result, fields) {
+//       if (err) throw err;
+//       console.log(result);
+//     });
+//   });
+
+
+
+
+/**================ Select Order By Query  ================== */
+
+
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     con.query("SELECT * FROM customers ORDER BY name", function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//     });
+//   });
+
+
+/**================ Mysql Limit Query  ================== */
 
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO customers (name, address) VALUES ?";
-    var values = [
-      ['John', 'Highway 71'],
-      ['Peter', 'Lowstreet 4'],
-      ['Amy', 'Apple st 652'],
-      ['Hannah', 'Mountain 21'],
-      ['Michael', 'Valley 345'],
-      ['Sandy', 'Ocean blvd 2'],
-      ['Betty', 'Green Grass 1'],
-      ['Richard', 'Sky st 331'],
-      ['Susan', 'One way 98'],
-      ['Vicky', 'Yellow Garden 2'],
-      ['Ben', 'Park Lane 38'],
-      ['William', 'Central st 954'],
-      ['Chuck', 'Main Road 989'],
-      ['Viola', 'Sideway 1633']
-    ];
-    con.query(sql, [values], function (err, result) {
+    var sql = "SELECT * FROM customers LIMIT 5";
+    con.query(sql, function (err, result) {
       if (err) throw err;
-      console.log("Number of records inserted: " + result.affectedRows);
+      console.log(result);
     });
   });
+
+
+
+/**================ Mysql UpdateQuery Query  ================== */
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log(result.affectedRows + " record(s) updated");
+//     });
+//   });
+
+
+/**================ Mysql Join Query  ================== */
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     var sql = "SELECT users.name AS user, products.name AS favorite FROM users JOIN products ON users.favorite_product = products.id";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//     });
+//   });
+
+
+  /**================ Delete Query  ================== */
+
+
+
+//   con.connect(function(err) {
+//     if (err) throw err;
+//     var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Number of records deleted: " + result.affectedRows);
+//     });
+//   });
+
+
+
+  /**================ Drop Table Query  ================== */
+
+
+//   con.connect(function(err) {
+//     if (err) throw err;
+//     var sql = "DROP TABLE customers";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Table deleted");
+//     });
+//   });
+
+
